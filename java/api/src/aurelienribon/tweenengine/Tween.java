904be9b74cc589872182eb0828a37aec68d518bb
nils <nstolpe@users.noreply.github.com>
2016-06-20 14:54:29 -0400

    testing once more

--- java/api/src/aurelienribon/tweenengine/Tween.java
+++ java/api/src/aurelienribon/tweenengine/Tween.java
@@ -110,7 +110,7 @@ public class Tween extends BaseTween<Tween> {
 	 * Gets the version number of the library.
 	 */
 	public static String getVersion() {
-		return "6.3.4";
+		return "6.3.3";
 	}
 
 	// -------------------------------------------------------------------------
